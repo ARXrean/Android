eff0c3ce195e152bf1bbce010951d713aa181de0
ARXrean <602052254@qq.com>
2017-03-05 04:00:50 +0800

    3.5:4:00

--- test/app/src/main/java/com/example/administrator/test/MainActivity.java
+++ test/app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.administrator.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String amount;
+    EditText totalAmount,persNbr;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        totalAmount=(EditText)findViewById(R.id.etAmount);
+        persNbr=(EditText)findViewById(R.id.etNbr);
+        display=(TextView)findViewById(R.id.tvAmountPerPerson);
+    }
+
+    public void btnCompute(View w){
+        Double total=Double.parseDouble(totalAmount.getText().toString());
+        Double persons=Double.parseDouble(persNbr.getText().toString());
+        Double share=total/persons;
+        share = Double.valueOf(Math.round(share+100));
+        share=share/100;
+
+        Toast.makeText(getBaseContext(),"the amount to show is "+share,Toast.LENGTH_SHORT).show();
     }
 }
